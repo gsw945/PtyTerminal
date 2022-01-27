18e2ec5c63aa51c4e5f5ecca121f5ec764bb6aa6
gsw945 <gsw945@foxmail.com>
2022-01-27 15:25:16 +0800

    websocket接入, xterm.js引入

---
+++ PtyWeb/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+
+namespace PtyWeb
+{
+    public class DemoController : WebApiController
+    {
+        [Route(HttpVerbs.Get, "/")]
+        public string GetDemoIndex() => "hello world";
+    }
+}
---
+++ PtyWeb/WebSocketPtyModule.cs
@@ -0,0 +1,45 @@
+﻿using EmbedIO.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PtyWeb
+{
+    public class WebSocketPtyModule : WebSocketModule
+    {
+        public WebSocketPtyModule(string urlPath, bool enableConnectionWatchdog = true) : base(urlPath, enableConnectionWatchdog)
+        {
+        }
+
+        protected override Task OnMessageReceivedAsync(IWebSocketContext context, byte[] buffer, IWebSocketReceiveResult result)
+        {
+            return SendToOthersAsync(context, Encoding.GetString(buffer));
+        }
+
+        protected override Task OnClientConnectedAsync(IWebSocketContext context)
+        {
+            // return base.OnClientConnectedAsync(context);
+            return Task.WhenAll(
+                base.OnClientConnectedAsync(context),
+                SendAsync(context, "Welcome to the chat room!"),
+                SendToOthersAsync(context, "Someone joined the chat room.")
+            );
+        }
+
+        protected override Task OnClientDisconnectedAsync(IWebSocketContext context)
+        {
+            // return base.OnClientDisconnectedAsync(context);
+            return Task.WhenAll(
+                SendToOthersAsync(context, "Someone left the chat room."),
+                base.OnClientDisconnectedAsync(context)
+            );
+        }
+
+        private Task SendToOthersAsync(IWebSocketContext context, string payload)
+        {
+            return BroadcastAsync(payload, c => c != context);
+        }
+    }
+}
